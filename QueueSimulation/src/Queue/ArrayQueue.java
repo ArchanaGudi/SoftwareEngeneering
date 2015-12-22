475a8c13da33b8367f00667a84ff15f377c33f60
archana <archana.gudi94@gmail.com>
2015-12-22 11:18:06 +0530

    Third Commit Enqueue and Dequeue methods using Array

--- QueueSimulation/src/Queue/ArrayQueue.java
+++ QueueSimulation/src/Queue/ArrayQueue.java
@@ -2,4 +2,38 @@ package Queue;
 
 public class ArrayQueue {
 
+	int rear;
+	int front;
+	int queue[];
+	int flag;
+	int qsize;
+
+	public ArrayQueue(int size) {
+		// TODO Auto-generated constructor stub
+		rear = -1;
+		front = -1;
+		qsize = size;
+		queue = new int[qsize];
+	}
+
+	int Enqueue(int value) {
+		if (rear == qsize)
+			flag = 0;
+		else {
+			rear++;
+			queue[rear] = value;
+			if (front == -1)
+				front = 0;
+			flag = 1;
+		}
+		return flag;
+	}
+	
+	int Dequeue(){
+		int value;
+		value=queue[front];
+		front++;
+		return value;
+		
+	}
 }
--- QueueSimulation/src/Queue/MainQueue.java
+++ QueueSimulation/src/Queue/MainQueue.java
@@ -5,7 +5,12 @@ public class MainQueue {
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-    
+		int size=10;
+		int data;
+		ArrayQueue que=new ArrayQueue(size);
+		que.Enqueue(5);
+		data=que.Dequeue();
+        System.out.println(data);
 		
 	}
 
